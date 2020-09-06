f09354bc3bed161330619a12936fc8184e055fc9
princemumo80@gmail.com <princemumo80@gmail.com>
2020-09-06 23:40:58 +0300

    add readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Prince Mumo.
+## Description:
+This is a webpage that allows users to order pizzas. People order from the website and are given th choice to have the products delivered to their houses or not. If not, then they go and pick the orders themselves.
+## Known Bugs
+There are no known bugs.
+## Technologies Used
+This projectis comprised of HTML, CSS and JavaScript.
+## Support and contact details
+In case you wish to contact me, send me an email, That is princemumo80@gmail.com
+### License
+*This work is under the GPL license. Feel free to make changes*
+Copyright (c) {2020} **{Prince Mumo.}**
--- js/script.js
+++ js/script.js
@@ -28,12 +28,12 @@ function getSize() {
   function totalAmount() {
     var totals = (getSize() + getCrust() + getTopping() + getCheese()) * getQuantity();
     alert(
-      "You have Ordered a total of " + getQuantity("") + " pizzas amounting to" + totals + "Thank you for ordering from PizzaCity. Welcome again."
+      "You have Ordered a total of " + getQuantity("") + " pizzas amounting to " + " Ksh " + totals + ". Thank you for ordering from PizzaCity. Welcome again."
       );
 
       /* Get the location of the customer */
       var location = prompt("If you want your pizza to be delivered to you please enter your location and if not cancel.");
       if (location !== "") {
-        alert("Your order will be delivered to" + location + " shortly. Delivery fee is 100/= and incase you didn't want it delivered please come for your order.");
+        alert("Your order will be delivered to " + location + " shortly. Delivery fee is 100/= and incase you didn't want it delivered please come for your order.");
       }
     };
